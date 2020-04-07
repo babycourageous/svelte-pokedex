8358bc415eab4ae78e47178ca255b05c118e2a01
Rene Dellefont <renedellefont@gmail.com>
2020-04-07 15:40:57 -0400

    Create helper components for PokemonDetail

---
+++ src/components/DataBar.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { tweened } from 'svelte/motion'
+
+  export let statColor
+  export let value
+
+  let amount = tweened(0)
+  $: amount.set(value)
+</script>
+
+<div
+  class="flex flex-col items-center justify-center text-white bg-{statColor}"
+  style="width: {$amount}%;"
+>
+  <span class="text-xs">{value}</span>
+</div>
